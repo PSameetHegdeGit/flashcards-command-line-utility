a9c5e4b28b24a2c636eb22213fc5033dd2bc618d
Sameet Hegde <sameethegde123@gmail.com>
2023-02-15 21:55:47 -0500

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,7 +1,34 @@
 from src.CommandLineInterface import *
 
+
+
 if __name__ == "__main__":
     storageapi = Storage()
     cli = CommandLineInterface(storageapi)
     cli.start()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+TODO
+
+(1) Flashcard set as Class
+(2) Storage API needs more transactional I/O operations
+    - Access all flashcards in flashcard_main.txt
+    - Extend fxnality to be able to access flashcards across all flash_card_XX.txt files
+    - 
+(3)  
+
+
+'''
