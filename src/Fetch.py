2c320893833605dd9b72a3400e48c75ce7353641
Sameet Hegde <sameethegde123@gmail.com>
2023-05-23 20:51:06 -0500

    csv

--- src/Fetch.py
+++ src/Fetch.py
@@ -1,7 +1,12 @@
 import csv
 import typing
-from CommandLineInterface import *
 import random
+
+
+class Entry:
+    def __init__(self, word: str, definition: str):
+        self.word = word
+        self.definition = definition
 
 
 
@@ -10,10 +15,31 @@ def get_entries_from_csv_file(csvfile: typing.IO):
     return [Entry(row[0], row[1]) for row in csv.reader(csvfile)]
 
 
+def get_random_set(entries: list[Entry]):
+    '''
+    randomly picks entries from list to generate a set consisting of 10 word, definition tuples
+
+    :param entries: list of words and their definition
+    :return: dict structured as word : definition
+    '''
+
+    noOfEntries = len(entries)
+    setOfFlashcards = {}
+
+    for _ in range(10):
+
+        entryIdx = random.randint(0, noOfEntries - 1)
+        entry = entries[entryIdx]
+        setOfFlashcards[entry.word] = entry.definition
+
+        entries[entryIdx], entries[-1] = entries[-1], entries[entryIdx]
+
+        entries = entries[:-1]
+
+    return setOfFlashcards
 
 
-class Entry:
-    def __init__(self, word: str, definition: str):
-        self.word = word
-        self.definition = definition
+
+
+
 
--- src/Tests/TestFetch.py
+++ src/Tests/TestFetch.py
@@ -3,8 +3,9 @@ from src.Fetch import *
 
 class TestFetch(unittest.TestCase):
 
-    def test_create_entries_from_csv_file(self, ):
-
-        with open()
-
-        get_entries_from_csv_file()
+    NotImplementedError
+    # def test_create_entries_from_csv_file(self, ):
+    #
+    #     with open()
+    #
+    #     get_entries_from_csv_file()
